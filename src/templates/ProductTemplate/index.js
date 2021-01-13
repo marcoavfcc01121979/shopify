d13b773d122469c94c4e0b28b996f6197161ba3d
marcoavfcc01121979 <marcoavfcc@gmail.com>
2021-01-12 23:40:51 -0300

    Dinamic query data

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+/* eslint-disable jsx-a11y/no-onchange */
+import React from 'react';
+import { graphql } from 'gatsby';
 import { Layout, ImageGallery } from 'components';
-import { Grid, SelectWrapper, Price } from './styles'
+import { Grid, SelectWrapper, Price } from './styles';
 import CartContext from 'context/CartContext';
+import { navigate, useLocation } from '@reach/router';
+import queryString from 'query-string';
 
 export const query = graphql`
   query ProductQuery($shopifyId: String) {
@@ -30,16 +33,23 @@ export default function ProductTemplate(props) {
   const { getProductById } = React.useContext(CartContext);
   const [ product, setProduct ] = React.useState(null);
   const [ selectedVariant, setSelectedVariant ] = React.useState(null);
+  const { search, origin, pathname } = useLocation();
+  console.log(search, origin, pathname)
+  const variantId = queryString.parse(search).variant;
 
   React.useEffect(() => {
     getProductById(props.data.shopifyProduct.shopifyId).then(result => {
       setProduct(result);
-      setSelectedVariant(result.variants[0]);
+      setSelectedVariant(result.variants.find(({id}) => id === variantId) || result.variants[0]);
     })
-  }, [getProductById, setProduct]);
+  }, [getProductById, setProduct, props.data.shopifyProduct.shopifyId, variantId]);
 
   const handleVariantChange = (e) => {
-    setSelectedVariant(product?.variants.find(v => v.id === e.target.value));
+    const newVariant = product?.variants.find(v => v.id === e.target.value);
+    setSelectedVariant(newVariant);
+    navigate(`${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`, {
+      replace: true,
+    })
   }
   return (
     <Layout>
@@ -47,22 +57,27 @@ export default function ProductTemplate(props) {
         <div>
           <h1>{props.data.shopifyProduct.title}</h1>
           <p>{props.data.shopifyProduct.description}</p>
-          {product?.availableForSale && (
+          {product?.availableForSale && !!selectedVariant && (
             <>
+              {product?.variants.length > 1 && 
               <SelectWrapper>
                 <strong>Variant</strong>
-                <select onChange={handleVariantChange}>
+                <select 
+                  value={selectedVariant.id} 
+                  onChange={handleVariantChange}
+                >
                   {product?.variants.map((v) => (
                     <option key={v.id} value={v.id}>{v.title}</option>
                   ))}
                 </select>
               </SelectWrapper>
-              {!!selectedVariant && <Price>E{selectedVariant.price}</Price>}
+              }
+              {!!selectedVariant && <Price>R$ {selectedVariant.price}</Price>}
             </>
           )}
         </div>
         <div>
-          <ImageGallery images={props.data.shopifyProduct.images} />
+          <ImageGallery selectedVariantImageId={selectedVariant?.image.id} images={props.data.shopifyProduct.images} />
         </div>
       </Grid>
     </Layout>
